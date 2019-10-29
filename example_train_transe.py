2ac12f4f0b47697f783a637c3e6cc3ece44753c9
Pedro Ferracini <pedro.ferracini@imusics.com>
2019-10-29 14:19:22 -0300

    Restaura modelos

--- example_train_transe.py
+++ example_train_transe.py
@@ -10,16 +10,16 @@ con = config.Config()
 con.set_in_path("./benchmarks/FB15K/")
 con.set_test_link_prediction(True)
 con.set_test_triple_classification(True)
-con.set_work_threads(4)
-con.set_train_times(500)
+con.set_work_threads(8)
+con.set_train_times(1000)
 con.set_nbatches(100)
-con.set_alpha(0.1)
-con.set_margin(1)
+con.set_alpha(0.001)
+con.set_margin(1.0)
 con.set_bern(0)
 con.set_dimension(100)
 con.set_ent_neg_rate(1)
 con.set_rel_neg_rate(0)
-con.set_opt_method("Adagrad")
+con.set_opt_method("SGD")
 
 #Models will be exported via tf.Saver() automatically.
 con.set_export_files("./res/model.vec.tf", 0)
@@ -28,7 +28,7 @@ con.set_out_files("./res/embedding.vec.json")
 #Initialize experimental settings.
 con.init()
 #Set the knowledge embedding model
-con.set_model(models.RESCAL)
+con.set_model(models.TransE)
 #Train the model.
 con.run()
 #To test models after training needs "set_test_flag(True)".
